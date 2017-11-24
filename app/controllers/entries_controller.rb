6071d548a4ce615bf535862fc1810def70196df6
spencer kyonka <spencer.kyonka@hotmail.ca>
2017-11-23 22:41:06 -0500

    do destroy

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -19,7 +19,30 @@ class EntriesController < ApplicationController
 
   def edit
     @entry = Entry.find(params[:id])
+    redirect_to entries_path
   end
+
+
+  def update
+    @entry = Event.find(params[:id])
+    # @entry.word = params[:entry][:word]
+    # @entry.definition = params[:entry][:definition]
+    # @entry.language = params[:entry][:language]
+    # @entry.location = params[:entry][:city]
+
+    if @entry.save
+      redirect_to event_url(params[:id])
+    # else
+    #   redirect_to event_url(params[:id])
+    end
+  end
+
+  def destroy
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+    redirect_to entries_path
+  end
+
 
 #<Entry id: 1, word: "absolute music", definition: "instrumental music not intended to represent or il...", language: "en",
 # created_at: "2017-11-22 03:04:07", updated_at: "2017-11-22 03:04:07">

fc4b7868c16df9d4e2cabca5ba1cb777b9a01004
spencer kyonka <spencer.kyonka@hotmail.ca>
2017-11-23 21:40:50 -0500

    add new

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -13,7 +13,10 @@ class EntriesController < ApplicationController
     render :new
   end
 
-
+  def created
+    redirect_to entries_url
+  end
+  
 
 #<Entry id: 1, word: "absolute music", definition: "instrumental music not intended to represent or il...", language: "en",
 # created_at: "2017-11-22 03:04:07", updated_at: "2017-11-22 03:04:07">

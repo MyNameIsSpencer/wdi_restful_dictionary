a1fd9dd9632d5173e5ef99aaadaaca0e8c3b6146
spencer kyonka <spencer.kyonka@hotmail.ca>
2017-11-23 22:17:16 -0500

    get edit working

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -16,7 +16,10 @@ class EntriesController < ApplicationController
   def created
     redirect_to entries_url
   end
-  
+
+  def edit
+    @entry = Entry.find(params[:id])
+  end
 
 #<Entry id: 1, word: "absolute music", definition: "instrumental music not intended to represent or il...", language: "en",
 # created_at: "2017-11-22 03:04:07", updated_at: "2017-11-22 03:04:07">
